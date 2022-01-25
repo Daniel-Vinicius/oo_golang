4dbc0915e94e570d3f44e89acb4cd5d136f8cfe1
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-01-25 16:17:10 -0300

    slices pt 2

--- slices/main.go
+++ slices/main.go
@@ -12,4 +12,16 @@ func main() {
 	fmt.Println(len(slice), "positions, actual length")
 	fmt.Println(cap(slice), "capacity, actual capacity of slice")
 	fmt.Println("The slice", slice)
+
+	for i := 0; i < 20; i++ {
+		slice = append(slice, i)
+		fmt.Println("Length:", len(slice))
+		fmt.Println("Capacity:", cap(slice))
+	}
+
+	sliceTest := slice
+	slice = append(slice, 43, 57, 63, 77)
+	slice[0] = 99
+	fmt.Println(slice)
+	fmt.Println(sliceTest)
 }
